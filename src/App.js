4a6cb3c00283f105d5152e84d12bc87af663f78d
Gastón Gorosito <gorosito.gaston@gmail.com>
2023-05-12 20:36:36 -0300

    correcciones

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React from "react"
 import './App.css';
 import Navbar from "./components/NavBar/index";
 import ItemListContainer from "./components/ItemListContainer/ItemListContainer";
-import ItemList from "./components/Card/ItemList";
+import Card from "./components/Card/Card"
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 
 
@@ -11,9 +12,13 @@ import ItemList from "./components/Card/ItemList";
 function App() {
   return (
     <div className="App">
-      <Navbar/>
-      <ItemListContainer/>
-  
+        <BrowserRouter>
+        <Navbar/>
+        <Routes>
+          <Route path="/" element={<ItemListContainer/>}/>
+          <Route path="/Card" element={<Card/>}/>
+        </Routes>
+        </BrowserRouter>
     </div>
   ); 
 }
--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -2,11 +2,11 @@ import React from "react";
 
 const Item = ( {product} ) => {
     return (
-        <div className="item">
-        <img src={product.image} alt="product image"/>
+        <div className="item card text-center">
+        <img  src={product.image} alt="product image"/>
             <div>
                 <h3>{product.title}</h3>
-
+                <a  className="btn btn-outline-secondary border-1 rounded-0">COMPRAR </a>
         </div>
             </div>
     )
