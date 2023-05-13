4736fa4aae1d9d984c0bf9c991bbede220089ef4
Gastón Gorosito <gorosito.gaston@gmail.com>
2023-05-12 22:53:53 -0300

    correcciones

--- src/App.js
+++ src/App.js
@@ -4,12 +4,21 @@ import Navbar from "./components/NavBar/index";
 import ItemListContainer from "./components/ItemListContainer/ItemListContainer";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Item from "./components/Item/Item";
-
-
-
-
+import {db} from "./components/Firebasefdx-shop/database"
+import {getDocs, collection} from "firebase/firestore"
 
 function App() {
+  const getProducts = async () => {
+ const productsCollection = collection (db, "products");
+ const productsDocsRef = await getDocs (productsCollection);
+ const productDocs = productsDocsRef.docs;
+ const products = productDocs 
+ .map((doc) => {
+   return {...doc.data(), id: doc.id};
+  })
+  console.log(products);
+  return products;
+}
   return (
     <div className="App">
         <BrowserRouter>
